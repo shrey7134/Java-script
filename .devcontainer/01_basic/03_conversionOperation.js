ae4fbf1a3416e81cdce2fb2839ad2bca57eb842a
shrey7134 <168639581+shrey7134@users.noreply.github.com>
2024-05-02 01:24:26 +0000

    operations

---
+++ .devcontainer/01_basic/03_conversionOperation.js
@@ -0,0 +1,65 @@
+/*
+let score="33abs"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+let score="33"
+//console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+
+let value=true;
+console.log(typeof value);
+
+let n=Number(value)
+console.log(typeof n);
+console.log(n);
+
+
+let value=35;
+let bool=Boolean(value)
+console.log(typeof bool);
+console.log(bool);
+
+
+
+let number=12345567
+let str=String(number)
+console.log(typeof str);
+console.log(str);
+
+*/
+// conversion into number
+
+
+// "33"=33
+// "33add"=NaN
+// true=1
+// false=0
+
+
+
+
+
+//  *****************************  operations *****************************
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+
+let str1="hello"
+let str2=" shrey"
+let str3=str1+str2;
+console.log(str3);
