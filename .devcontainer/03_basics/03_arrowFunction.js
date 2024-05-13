2b99e058d962ed7cd9ccdfa5eac66d3c2287cb60
shrey7134 <168639581+shrey7134@users.noreply.github.com>
2024-05-13 10:34:38 +0000

    arrow

---
+++ .devcontainer/03_basics/03_arrowFunction.js
@@ -0,0 +1,79 @@
+// const user={
+//     userName:"hitesh",
+//     price:999,
+
+//     welcomeMessage:function(){
+//         console.log(`${this.userName} , welcome to website`);
+//         console.log(this);
+//     }
+// }
+// // user.welcomeMessage()
+// // user.userName="sam"
+// // user.welcomeMessage()
+
+// // function chai(){
+// //     console.log(this);
+// // }
+
+// // chai()
+
+// // const chai=() =>{
+// //     let userName="shrey"
+// //     console.log(this.userName);
+// // }
+// chai()
+
+// const user = {
+//     username: "hitesh",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
+
+// }
+
+// // user.welcomeMessage()
+// // user.username = "sam"
+// // user.welcomeMessage()
+
+// // console.log(this);
+
+// // function chai(){
+// //     let username = "hitesh"
+// //     console.log(this.username);
+// // }
+
+// // chai()
+
+// // const chai = function () {
+// //     let username = "hitesh"
+// //     console.log(this.username);
+// // }
+
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);
+// }
+
+
+// // chai()
+
+// // const addTwo = (num1, num2) => {
+// //     return num1 + num2
+// // }
+
+// // const addTwo = (num1, num2) =>  num1 + num2
+
+// // const addTwo = (num1, num2) => ( num1 + num2 )
+
+// const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+// console.log(addTwo(3, 4))
+
+
+// // const myArray = [2, 5, 3, 7, 8]
+
+// // myArray.forEach()
