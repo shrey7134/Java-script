17104466e0291bc49d7ccbda751bb34cc8dc08fd
shrey7134 <168639581+shrey7134@users.noreply.github.com>
2024-05-13 13:26:38 +0000

    l

---
+++ .devcontainer/04_Control_Flow/switch.js
@@ -0,0 +1,37 @@
+month=3;
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    default:
+        break;
+}// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
